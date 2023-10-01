f66d4f3c5d3dfc3045a8cece0fb309abecd933a1
Tulio Macedo <tuliomac@gmail.com>
2023-10-01 09:25:09 -0300

    🔧 Configs
    adding configs to new site

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
