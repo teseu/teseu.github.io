86a471d2326754aa52c2e23ec06376850db17de5
Tulio Macedo <tuliomac@gmail.com>
2023-10-01 14:58:04 -0300

    💄 Home background
    images for home view light and dark

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,10 +6,19 @@
  <main>
     <div class="flex">
       <img alt="Teseu logo" class="logo w-48 h-48" :src="Teseu" />
-      <h1 class="flex text-7xl w-full justify-center items-center pb-6">Página do Teseu</h1>
+      <h1 class="flex text-7xl text-info-1100 w-full justify-center items-center pb-6">Página do Teseu</h1>
     </div>
  </main>
 </template>
 <style scoped>
- 
+  main {
+    background-repeat: no-repeat;
+    background-size: 100vw calc(100vh - 10rem);
+  }
+ [data-theme="light"] main {
+  background-image: url('/F26-day.png');
+ }
+ [data-theme="dark"] main {
+  background-image: url('/F26-night.png');
+ }
 </style>
