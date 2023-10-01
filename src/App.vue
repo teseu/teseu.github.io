f56c89138c7d3b4b2a4830989585bd26a85cd13b
Tulio Macedo <tuliomac@gmail.com>
2023-10-01 17:41:26 -0300

    🐛 Layout
    corrections in layout

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,22 @@ import Footer from '@/components/TheFooter.vue'
 </script>
 
 <template>
-  <div class="h-screen">
+  <div class="flex flex-col">
+  <div class="w-full fixed z-20">
     <header>
-      <div class="flex items-center justify-between w-full px-[20rem] bg-bw-50 h-20 fixed z-20">
+      <div class="flex items-center justify-between w-full px-[20rem] bg-bw-50 h-20">
         <Header />
       </div>
     </header>
-    <RouterView class="px-[20rem] pt-8 pb-8 bg-bw-100 top-20 text-bw-900 h-[calc(100vh-10rem)] relative z-10" />
-    <footer>
-      <div class="w-full px-[20rem] bg-bw-200 h-20 top-20 relative">
+  </div>
+  <div class="flex flex-col relative z-10 top-20">
+    <RouterView class="px-[20rem] pt-8 pb-8 bg-bw-100 top-20 text-bw-900 min-h-[calc(100vh-10rem)]" onclick="closeDrawer('left')" />
+    <footer class="w-full px-[20rem] bg-bw-200 h-20">
+      <div>
         <Footer />
       </div>
     </footer>
+  </div>
   </div>
 </template>
 
