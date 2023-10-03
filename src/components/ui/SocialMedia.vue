a3b4d47fb63e9e8f76f8b7e776fbd85ddac9e04e
Tulio Macedo <tuliomac@gmail.com>
2023-10-03 08:36:35 -0300

    ♻️ Router links
    switching router link to regular anchors in social media links

--- src/components/ui/SocialMedia.vue
+++ src/components/ui/SocialMedia.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { RouterLink } from 'vue-router'
  import Gitlab from '../icons/GitlabIcon.vue'
  import Github from '../icons/GithubIcon.vue'
  import Blog from '../icons/BlogIcon.vue'
@@ -12,12 +11,12 @@ import { RouterLink } from 'vue-router'
   <div>
     <div class="text-xl mb-3">Você pode me encontrar aqui também</div>
     <div class="flex items-center gap-4 text-info-1050">
-      <RouterLink to="https://gitlab.com/teseu" target="_blank"> <Gitlab class="w-8" /></RouterLink>
-      <RouterLink to="https://github.com/teseu" target="_blank"> <Github class="w-8" /></RouterLink>
-      <RouterLink to="#" target="_blank"> <Blog class="w-8" /></RouterLink>
-      <RouterLink to="https://mastodon.social/@teseu" target="_blank"> <Mastodon class="w-8" /></RouterLink>
-      <RouterLink to="http://twitter.com/tuliomac" target="_blank"> <Twitter class="w-8" /></RouterLink>
-      <RouterLink to="https://vaporhole.xyz/~teseu/" target="_blank"> <Vapor class="w-8" /></RouterLink>
+      <a href="https://gitlab.com/teseu" target="_blank"> <Gitlab class="w-8" /></a>
+      <a href="https://github.com/teseu" target="_blank"> <Github class="w-8" /></a>
+      <a href="#" target="_blank"> <Blog class="w-8" /></a>
+      <a href="https://mastodon.social/@teseu" target="_blank"> <Mastodon class="w-8" /></a>
+      <a href="http://twitter.com/tuliomac" target="_blank"> <Twitter class="w-8" /></a>
+      <a href="https://vaporhole.xyz/~teseu/" target="_blank"> <Vapor class="w-8" /></a>
     </div>
   </div>
 </template>
