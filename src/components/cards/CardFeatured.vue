137eed65671ded3a343d008192d24fe80d13034c
Tulio Macedo <tuliomac@gmail.com>
2023-10-07 09:14:06 -0300

    ✨ Featured view
    view for who page

---
+++ src/components/cards/CardFeatured.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  featured: Object,
+});
+</script>
+
+<template>
+  <div class="overflow-hidden rounded-lg has-shadow w-80">
+    <img
+      :src="'/src/assets/img/quem/' + props.featured.img"
+      class="aspect-video max-h-[19rem] rounded-b-none"
+      :alt="props.featured.alt"
+    />
+    <div class="p-4 flex flex-col gap-2">
+      <slot name="badge"></slot>
+      <h3 class="text-xl font-semibold">{{ props.featured.nome }}</h3>
+      <div class="text-sm">
+        <p>{{ props.featured.papel }}</p>
+      </div>
+      <div class="flex gap-2 text-info-1050">
+        <slot name="redes"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped></style>
--- src/views/FeaturedView.vue
+++ src/views/FeaturedView.vue
@@ -1,6 +1,80 @@
+<script setup>
+  import Title from '@/components/ui/TheTitle.vue'
+import Subtitle from '@/components/ui/TheSubtitle.vue'
+import Card from '../components/cards/CardFeatured.vue'
+import { PhFacebookLogo, PhGithubLogo, PhGitlabLogo, PhTwitterLogo } from '@phosphor-icons/vue'
+
+const featured = [
+  {
+    "id": 1,
+    "img": "lud.jpg",
+    "alt": "Ludmilla",
+    "nome": "Ludmilla Bizzotto Petrin",
+    "papel": "😍 Suporte e mais 💥"
+  },
+  {
+    "id": 2,
+    "img": "teseu.png",
+    "alt": "Teseu",
+    "nome": "Teseu",
+    "papel": "🖥️ Querendo ser coder"
+  },
+  {
+    "id": 3,
+    "img": "tulio.jpg",
+    "alt": "Tulio e Helena",
+    "nome": "Tulio Macedo",
+    "papel": "Eu e Helena ❤️"
+  }
+]
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="">
+    <Title>
+      <template #titulo>Quem</template>
+    </Title>
+    <Subtitle>
+      <template #titulo>Para que tudo possa acontecer, sempre tem alguém, pode ser na linha de frente ou nos bastidores</template>
+    </Subtitle>
+    <div class="flex flex-wrap justify-between gap-4 items-stretch">
+        <Card
+        :featured="featured[0]">
+          <template #badge>
+            <div class="flex gap-4">
+              <div class="badge solid info cornered">Mãe</div>
+              <div class="badge solid success cornered">Segurança</div>
+              <div class="badge solid danger cornered">Confusão</div>
+            </div>
+          </template>
+          <template #redes>
+            <a href="https://www.facebook.com/ludmilla.bizzottopetrin" target="_blank"><ph-facebook-logo :size="32" weight="duotone" /></a>
+          </template>
+        </Card>
+        <Card
+        :featured="featured[1]">
+        <template #badge>
+            <div class="flex gap-4">
+              <div class="badge solid warn cornered">Em desenvolvimento</div>
+            </div>
+          </template>
+          <template #redes>
+            <a href="http://github.com/tuliomac" target="_blank"><ph-github-logo :size="32" weight="duotone" /></a>
+            <a href="http://gitlab.com/teseu" target="_blank"><ph-gitlab-logo :size="32" weight="duotone" /></a>
+          </template>
+        </Card>
+        <Card
+        :featured="featured[2]"> 
+        <template #badge>
+            <div class="flex gap-4">
+              <div class="badge solid info cornered">Pai</div>
+              <div class="badge solid warn cornered">Em desenvolvimento</div>
+            </div>
+          </template>
+          <template #redes>
+            <a href="https://twitter.com/tuliomac" target="_blank"><ph-twitter-logo :size="32" weight="duotone" /></a>
+          </template>
+        </Card>
+    </div>
   </div>
 </template>
 
