1f74d9a5971c356b9c2c449e56fd9f03a90d0999
Tulio Macedo <tuliomac@gmail.com>
2023-10-07 09:15:33 -0300

    ✨ Menu
    creating a menu for links

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,9 +1,10 @@
 <script setup>
+import { RouterLink } from 'vue-router'
 import { useThemeStore } from '@/stores/theme.js'
 import {ref} from 'vue'
 
-import Drawer from '@/components/ui/TheDrawer.vue'
-import Search from '@/components/ui/TheSearch.vue'
+// import Drawer from '@/components/ui/TheDrawer.vue'
+// import Search from '@/components/ui/TheSearch.vue'
 
 const theme = useThemeStore()
 
@@ -12,8 +13,8 @@ const dataContent = ref(theme.dataContent)
 
 <template>
   <div class="flex justify-between w-full">
-    <nav class="flex flex-wrap gap-4">
-      <div class="p-3 space-y-2 bg-info-600 cursor-pointer rounded shadow" onclick="toggleDrawer('left')">
+    <nav class="flex flex-wrap items-center gap-4">
+      <!-- <div class="p-3 space-y-2 bg-info-600 cursor-pointer rounded shadow" onclick="toggleDrawer('left')">
         <span class="block w-6 h-0.5 bg-info-1000 animate-pulse"></span>
         <span class="block w-6 h-0.5 bg-info-1000 animate-pulse"></span>
         <span class="block w-6 h-0.5 bg-info-1000 animate-pulse"></span>
@@ -21,11 +22,18 @@ const dataContent = ref(theme.dataContent)
       <div>
         <label class="drawer-overlay" onclick="toggleDrawer('left')"></label>
         <Drawer />
-      </div>
-      <div class="w-40 h-[2.3rem] logo" ></div>
+      </div> -->
+      <RouterLink to="/" class="mr8" >
+        <div class="w-40 h-16 logo"></div>
+      </RouterLink>
+      <RouterLink to="frases">frases</RouterLink>
+      <RouterLink to="lembrancas">lembranças</RouterLink>
+      <RouterLink to="sobre">sobre</RouterLink>
+      <RouterLink to="quem">quem</RouterLink>
+      <RouterLink to="contato">contato</RouterLink>
     </nav>
     <div class="flex items-center">
-      <Search />
+      <!-- <Search /> -->
       <input checked class="switch ghost success" :data-content="dataContent" type="checkbox" id="theme-toggle" onclick="toggleDarkMode()" />
     </div>
   </div>
@@ -33,12 +41,12 @@ const dataContent = ref(theme.dataContent)
 <style scoped>
 .logo {
   background-repeat: no-repeat;
-  background-size: 10rem 2.3rem;
+  background-size: contain;
 }
-/* [data-theme="light"] .logo {
-  background-image: url("@/assets/img/logo.png");
+[data-theme="light"] .logo {
+  background-image: url("@/assets/img/teseu-logo.png");
 }
 [data-theme="dark"] .logo {
-  background-image: url("@/assets/img/logoBranca.png");
-} */
+  background-image: url("@/assets/img/teseu-logo-clara.png");
+}
 </style>
