e50de1a192cd9cdec5ebb3f36fd7c71deb633e2f
Tulio Macedo <tuliomac@gmail.com>
2023-10-07 09:12:54 -0300

    💄 Cores
    more adjustments

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,14 @@ import Footer from '@/components/TheFooter.vue'
   <div class="flex flex-col">
   <div class="w-full fixed z-20">
     <header>
-      <div class="flex items-center justify-between w-full px-[20rem] bg-bw-50 h-20">
+      <div class="flex items-center justify-between w-full px-[20rem] bg-info-100 h-20">
         <Header />
       </div>
     </header>
   </div>
   <div class="flex flex-col relative z-10 top-20">
-    <RouterView class="px-[20rem] pt-8 pb-8 bg-bw-100 top-20 text-bw-900 min-h-[calc(100vh-10rem)]" onclick="closeDrawer('left')" />
-    <footer class="w-full px-[20rem] bg-bw-200 h-20">
+    <RouterView class="px-[20rem] pt-8 pb-8 bg-info-200 top-20 text-bw-900 min-h-[calc(100vh-10rem)]" onclick="closeDrawer('left')" />
+    <footer class="w-full px-[20rem] bg-info-100 h-20">
       <div>
         <Footer />
       </div>
