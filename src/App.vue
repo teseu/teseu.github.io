605b489682139876031f348da933d256dcdce59a
Tulio Macedo <tuliomac@gmail.com>
2023-10-08 07:16:23 -0300

    ✨ Card view
    to business card, without header nor footer

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <script setup>
-import {RouterView} from 'vue-router'
+import { RouterView } from 'vue-router'
 import Header from '@/components/TheHeader.vue'
 import Footer from '@/components/TheFooter.vue'
 </script>
 
 <template>
   <div class="flex flex-col">
-  <div class="w-full fixed z-20">
-    <header>
-      <div class="flex items-center justify-between w-full px-[20rem] bg-info-100 h-20">
-        <Header />
-      </div>
-    </header>
-  </div>
-  <div class="flex flex-col relative z-10 top-20">
-    <RouterView class="px-[20rem] pt-8 pb-8 bg-info-200 top-20 text-bw-900 min-h-[calc(100vh-10rem)]" onclick="closeDrawer('left')" />
-    <footer class="w-full px-[20rem] bg-info-100 h-20">
-      <div>
-        <Footer />
-      </div>
-    </footer>
-  </div>
+    <div v-if="$route.name != 'card'" class="w-full fixed z-20">
+      <header>
+        <div class="flex items-center justify-between w-full px-8 md:px-16 xl:px-[20rem] bg-info-100 h-20">
+          <Header />
+        </div>
+      </header>
+    </div>
+    <div class="flex flex-col relative z-10 top-20">
+      <RouterView class="px-8 md:px-16 xl:px-[20rem] pt-8 pb-8 bg-info-200 top-20 text-bw-900 min-h-[calc(100vh-10rem)]"
+        onclick="closeDrawer('left')" />
+      <footer v-if="$route.name != 'card'" class="w-full px-8 md:px-16 xl:px-[20rem] bg-info-100 h-20">
+        <div>
+          <Footer />
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
 
---
+++ src/views/BusinessCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Title from '@/components/ui/TheTitle.vue'
+import Subtitle from '@/components/ui/TheSubtitle.vue'
+import Social from '@/components/ui/SocialMedia.vue'
+</script>
+<template>
+  <div class="flex flex-col items-center">
+    <div class="py-8">
+      <Title>
+        <template #titulo>Tulio Macedo</template>
+      </Title>
+      <Subtitle>
+        <template #titulo>Web Developer, Free software enthusiast, Fedora user</template>
+      </Subtitle>
+    </div>
+    <div class="py-8">
+      <img class="w-48 h-48 rounded-full object-cover object-top" src="@/assets/img/quem/tulio-profile.png" alt="Tulio">
+    </div>
+    <div>
+      <Social />
+    </div>
+  </div>
+</template>
+<style scoped></style>
