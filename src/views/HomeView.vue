c8a4e1cc7bbdce292f4c96113c129af88e8e1b76
Tulio Macedo <tuliomac@gmail.com>
2023-10-12 09:19:30 -0300

    🚀 Home
    adjusting responsiveness

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,27 @@
 <script setup>
- import Teseu from '@/assets/img/teseu.png'
+import Teseu from '@/assets/img/teseu.png'
 </script>
 
 <template>
  <main>
-    <div class="flex">
-      <img alt="Teseu logo" class="logo w-48 h-48" :src="Teseu" />
-      <h1 class="flex text-7xl text-info-1100 w-full justify-center items-center pb-6">Página do Teseu</h1>
-    </div>
+  <div class="flex flex-col lg:flex-row gap-8">
+   <img alt="Teseu logo" class="logo w-48 h-48" :src="Teseu" />
+   <h1 class="flex text-4xl sm:text-5xl md:text-7xl text-info-1100 w-full justify-center items-center pb-6">Página do
+    Teseu</h1>
+  </div>
  </main>
 </template>
 <style scoped>
-  main {
-    background-repeat: no-repeat;
-    background-size: 100vw calc(100vh - 10rem);
-  }
- [data-theme="light"] main {
-  background-image: url('/F26-day.png');
- }
- [data-theme="dark"] main {
-  background-image: url('/F26-night.png');
- }
+main {
+ background-repeat: no-repeat;
+ background-size: 100vw calc(100vh - 10rem);
+}
+
+[data-theme="light"] main {
+ background-image: url('/F26-day.png');
+}
+
+[data-theme="dark"] main {
+ background-image: url('/F26-night.png');
+}
 </style>
