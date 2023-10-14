469fbf46cb7b2de0d6a79560ff9f0fb71af24d35
Tulio Macedo <tuliomac@gmail.com>
2023-10-14 09:58:49 -0300

    🚧 Gallery
    one more step, adding all the images to the view

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,12 +1,18 @@
 <script setup>
   import Title from '@/components/ui/TheTitle.vue'
   import Subtitle from '@/components/ui/TheSubtitle.vue'
-  import Lud from '@/assets/img/lud-gravida.jpg'
-  import Parto from '@/assets/img/entrada-parto.jpg'
-  import HelenaMaternidade from '@/assets/img/helena-maternidade.jpg'
-  import HelenaEu from '@/assets/img/helena-eu.jpg'
-  import NosSelfie from '@/assets/img/nos-selfie.jpg'
-  import HelenaArvore from '@/assets/img/helena-arvore.jpg'
+  import Lud from '@/assets/img/lembrancas/lud-gravida.jpg'
+  import Parto from '@/assets/img/lembrancas/entrada-parto.jpg'
+  import HelenaMaternidade from '@/assets/img/lembrancas/helena-maternidade.jpg'
+  import HelenaEu from '@/assets/img/lembrancas/helena-eu.jpg'
+  import HelenaEu2 from '@/assets/img/lembrancas/helena-eu-2.jpg'
+  import NosSelfie from '@/assets/img/lembrancas/nos-selfie.jpg'
+  import NosSelfie2 from '@/assets/img/lembrancas/nos-selfie-2.jpg'
+  import HelenaArvore from '@/assets/img/lembrancas/helena-arvore.jpg'
+  import Charrete from '@/assets/img/lembrancas/charrete.jpg'
+  import Anonovo from '@/assets/img/lembrancas/ano-novo2020.png'
+  import Pais from '@/assets/img/lembrancas/pais.jpg'
+  import Trem from '@/assets/img/lembrancas/trem-fantasma.jpg'
 </script>
 <template>
   <div class="">
@@ -22,82 +28,160 @@
     <div class="mx-auto max-w-screen-2xl">
         
 
-        <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8 grid-rows-6">
+        <div class="grid grid-cols-3 gap-4md:gap-6 xl:gap-8 auto-rows-[minmax(18rem,_1fr)]">
             <!-- image - start -->
             <a href="#"
-                class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
-                <img :src="Lud" loading="lazy" alt="Lud grávida" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 saturate-50 hover:saturate-100 group-hover:scale-110" />
+                class="group link">
+                <img :src="Lud" loading="lazy" alt="Lud grávida" class="img" />
 
                 <div
-                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                    class="img-div">
                 </div>
 
-                <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">VR</span>
+                <span class="legend">Gravidez</span>
             </a>
             <!-- image - end -->
 
             <!-- image - start -->
             <a href="#"
-                class="group relative flex items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:row-span-2">
-                <img :src="Parto" loading="lazy" alt="Photo by Magicle" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 saturate-50 hover:saturate-100 group-hover:scale-110" />
+                class="group link">
+                <img :src="Parto" loading="lazy" alt="Lud e Tulio na entrada do centro cirúrgico, antes do parto" class="img" />
 
                 <div
-                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                    class="img-div">
                 </div>
 
-                <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Tech</span>
+                <span class="legend">Dia do parto</span>
             </a>
             <!-- image - end -->
 
             <!-- image - start -->
             <a href="#"
-                class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80">
-                <img :src="HelenaMaternidade" loading="lazy" alt="Photo by Martin Sanchez" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 saturate-50 hover:saturate-100 group-hover:scale-110" />
+                class="group link">
+                <img :src="HelenaMaternidade" loading="lazy" alt="Helena na saída da maternidade" class="img" />
 
                 <div
-                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                    class="img-div">
                 </div>
 
-                <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Dev</span>
+                <span class="legend">Saindo da maternidade</span>
             </a>
             <!-- image - end -->
 
             <!-- image - start -->
             <a href="#"
-                class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
-                <img :src="HelenaEu" loading="lazy" alt="Photo by Lorenzo Herrera" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 saturate-50 hover:saturate-100 group-hover:scale-110" />
+                class="group link">
+                <img :src="HelenaEu" loading="lazy" alt="Helena no colo do Túlio em frente à casa da avó" class="img" />
 
                 <div
-                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                    class="img-div">
                 </div>
 
-                <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Retro</span>
+                <span class="legend">Visitando vovó</span>
             </a>
             <!-- image - end -->
 
             <!-- image - start -->
             <a href="#"
-                class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80 md:col-span-2">
-                <img :src="NosSelfie" loading="lazy" alt="Photo by Lorenzo Herrera" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 saturate-50 hover:saturate-100 group-hover:scale-110" />
+                class="group link">
+                <img :src="NosSelfie" loading="lazy" alt="Selfie com Lud, Helena e Tulio" class="img" />
 
                 <div
-                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                    class="img-div">
                 </div>
 
-                <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Retro</span>
+                <span class="legend">Saindo pra jantar</span>
             </a>
             <!-- image - end -->
 
             <!-- image - start -->
             <a href="#"
-                class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
-                <img :src="HelenaArvore" loading="lazy" alt="Photo by Lorenzo Herrera" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 saturate-50 hover:saturate-100 group-hover:scale-110" />
+                class="group link">
+                <img :src="HelenaArvore" loading="lazy" alt="Helena explorando o mundo, ao lado de um árvore" class="img" />
 
                 <div
-                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                    class="img-div">
                 </div>
 
-                <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Retro</span>
+                <span class="legend">Explorando o mundo</span>
+            </a>
+            <!-- image - end -->
+
+            <!-- image - start -->
+            <a href="#"
+                class="group link">
+                <img :src="HelenaEu2" loading="lazy" alt="Selfie de Tulio com Helena no colo" class="img" />
+
+                <div
+                    class="img-div">
+                </div>
+
+                <span class="legend">Selfie</span>
+            </a>
+            <!-- image - end -->
+
+            <!-- image - start -->
+            <a href="#"
+                class="group link">
+                <img :src="NosSelfie2" loading="lazy" alt="Selfie com Lud, Helena e Tulio" class="img" />
+
+                <div
+                    class="img-div">
+                </div>
+
+                <span class="legend">Na escola</span>
+            </a>
+            <!-- image - end -->
+
+            <!-- image - start -->
+            <a href="#"
+                class="group link">
+                <img :src="Charrete" loading="lazy" alt="Helena e Tulio em um passeio de charrete" class="img" />
+
+                <div
+                    class="img-div">
+                </div>
+
+                <span class="legend">Passeio de charrete</span>
+            </a>
+            <!-- image - end -->
+
+            <!-- image - start -->
+            <a href="#"
+                class="group link">
+                <img :src="Anonovo" loading="lazy" alt="Foto de comemoração do ano novo de 202 com Helena, Lud e Tulio" class="img" />
+
+                <div
+                    class="img-div">
+                </div>
+
+                <span class="legend">Ano novo de 2020</span>
+            </a>
+            <!-- image - end -->
+
+            <!-- image - start -->
+            <a href="#"
+                class="group link">
+                <img :src="Trem" loading="lazy" alt="Selfie com Helena e Tulio na entrada do trem fantasma" class="img" />
+
+                <div
+                    class="img-div">
+                </div>
+
+                <span class="legend">Medo do trem fantasma</span>
+            </a>
+            <!-- image - end -->
+
+            <!-- image - start -->
+            <a href="#"
+                class="group link">
+                <img :src="Pais" loading="lazy" alt="Helena entregando a Tulio presente do dia dos pais" class="img" />
+
+                <div
+                    class="img-div">
+                </div>
+
+                <span class="legend">Dia dos pais na escola</span>
             </a>
             <!-- image - end -->
         </div>
@@ -106,6 +190,21 @@
   </div>
 </template>
 
-<style>
+<style scoped>
+.link {
+    @apply relative flex items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg;
+}
+
+.img {
+    @apply absolute inset-0 h-full w-full object-cover object-center transition duration-200 saturate-50 hover:saturate-100 group-hover:scale-110;
+}
+
+.img-div {
+    @apply pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50;
+}
+
+.legend {
+    @apply relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg;
+}
 
 </style>
