c1b3aa06e5f7e1a42c5c003df51c7733ea775a70
Tulio Macedo <tuliomac@gmail.com>
2023-10-15 08:42:52 -0300

    ⚡️ Contact view
    improving contact view

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -15,32 +15,41 @@ import { PhEnvelope, PhPhone, PhChatCenteredText, PhUserCircle, PhPaperPlaneTilt
       data-protonpass-form-type="register,recovery">
       <div class="grid grid-cols-1 sm:grid-cols-2 gap-8">
         <div class="input text-xl leading-10 h-20 info outline">
-          <PhUserCircle :size="36" weight="duotone" />
+          <PhUserCircle :size="36" weight="duotone" class="text-info-1050" />
           <input name="nome" required type="text" placeholder="Nome *">
         </div>
         <div class="input text-xl leading-10 h-20 info outline">
-          <PhEnvelope :size="36" weight="duotone" />
+          <PhEnvelope :size="36" weight="duotone" class="text-info-1050" />
           <input name="email" required type="text" placeholder="E-mail *">
         </div>
         <div class="input text-xl leading-10 h-20 info outline">
-          <PhPhone :size="36" weight="duotone" />
+          <PhPhone :size="36" weight="duotone" class="text-info-1050" />
           <input name="telefone" type="text" placeholder="Telefone">
         </div>
       </div>
       <div class="mt-8">
         <div class="input text-xl leading-10 h-20 info outline">
-          <PhChatCenteredText :size="36" weight="duotone" />
+          <PhChatCenteredText :size="36" weight="duotone" class="text-info-1050" />
           <input name="mensagem" required type="text" placeholder="Mensagem *" />
         </div>
       </div>
       <div class="mt-8">
-        <button class="btn solid info px-14">
-          <PhPaperPlaneTilt :size="36" weight="duotone" />
-          <p>Enviar</p>
+        <button class="btn solid info bg-info-1050 px-14">
+          <PhPaperPlaneTilt :size="36" weight="duotone" class="text-info-300" />
+          <p class="text-info-300 text-xl">Enviar</p>
         </button>
       </div>
     </form>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.info input::placeholder,
+.text-info-1050 {
+  color: var(--sira-colors-info-1050) !important;
+}
+
+.bg-info-1050 {
+  background-color: var(--sira-colors-info-1050) !important;
+}
+</style>
