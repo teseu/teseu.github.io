c9ffb9c51cd716db6e0e6407110b5330f8b19e22
Tulio Macedo <tuliomac@gmail.com>
2023-10-15 08:45:40 -0300

    ⚡️ Bussiness card
    ajusting theme suport and language

---
+++ src/components/icons/ThemeSwitch.vue
@@ -0,0 +1,17 @@
+<script setup>
+ 
+</script>
+
+<template>
+ <div class="w-40 items-center text-3xl relative cursor-pointer toggle-theme" onclick="toggleDarkMode()"
+        id="theme-toggle">
+        <span class="opacity-0 dark:opacity-100 toggle-theme absolute">☀️</span>
+        <span class="opacity-100 dark:opacity-0 toggle-theme absolute">🌙</span>
+      </div>
+</template>
+<style scoped>
+ .toggle-theme {
+  @apply flex transition-all duration-500 delay-75;
+}
+
+</style>
--- src/views/BusinessCard.vue
+++ src/views/BusinessCard.vue
@@ -1,16 +1,45 @@
 <script setup>
+import { ref } from 'vue'
 import Title from '@/components/ui/TheTitle.vue'
 import Subtitle from '@/components/ui/TheSubtitle.vue'
 import Social from '@/components/ui/SocialMedia.vue'
+import Theme from '@/components/icons/ThemeSwitch.vue'
+
+const languagePTBR = ref(true)
+
 </script>
 <template>
   <div class="flex flex-col items-center">
-    <div class="py-8">
+    <div class="flex gap-8">
+    <div class="flex gap-8">
+        <Theme />
+      <div class="flex gap-8 text-5xl transition-all duration-300" @click="languagePTBR = !languagePTBR">
+        <div class="toggle-language">
+          🇧🇷
+          <span :class="{ active: languagePTBR }"></span>
+        </div>
+        <div class="toggle-language">
+          🇺🇸
+          <span :class="{ active: !languagePTBR }"></span>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div class="py-8 transition-all duration-300 delay-150">
       <Title>
         <template #titulo>Tulio Macedo</template>
       </Title>
       <Subtitle>
-        <template #titulo>Web Developer, Free software enthusiast, Fedora user</template>
+        <template #titulo>
+        <div class="transition-all duration-300 delay-150">
+          <div v-show="languagePTBR">
+            Desenvolvedor Web, entusiasta do Software Livre, usuário Fedora
+          </div>
+          <div v-show="!languagePTBR">
+            Web Developer, Free software enthusiast, Fedora user
+          </div>
+        </div>
+        </template>
       </Subtitle>
     </div>
     <div class="py-8">
@@ -21,4 +50,12 @@ import Social from '@/components/ui/SocialMedia.vue'
     </div>
   </div>
 </template>
-<style scoped></style>
+<style scoped>
+.toggle-language {
+  @apply flex flex-col transition-all duration-300;
+}
+
+.active {
+  @apply bg-info-600 rounded-xl w-full h-1;
+}
+</style>
