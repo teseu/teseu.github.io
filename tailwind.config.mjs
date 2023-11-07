98e6529bebaa9f58dac7be44c3bda61ac5087fa1
Tulio Macedo <tuliomac@gmail.com>
2023-11-07 14:09:50 -0300

    ✨ Temas
    inclusão de daisyui e temas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,55 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('daisyui')],
+	daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require('daisyui/src/theming/themes')['[data-theme=dark]'],
+          '.bg-color-1': {
+            'background-color': '#1f2937'
+          },
+          primary: '#a5b4fc',
+          '--primary-dark': '#312e81',
+          '.text-primary-dark': {
+            color: 'var(--primary-dark)'
+          },
+          '.bg-primary-dark': {
+            'background-color': 'var(--primary-dark)'
+          }
+        },
+        corporate: {
+          ...require('daisyui/src/theming/themes')['[data-theme=corporate]'],
+          '.bg-color-1': {
+            'background-color': '#f1f5f9'
+          },
+          '--primary-dark': '#001c9b',
+          '.text-primary-dark': {
+            color: 'var(--primary-dark)'
+          },
+          '.bg-primary-dark': {
+            'background-color': 'var(--primary-dark)'
+          }
+        },
+        night: {
+          ...require('daisyui/src/theming/themes')['[data-theme=night]'],
+          '.bg-color-1': {
+            'background-color': '#1e293b'
+          },
+          '--primary-dark': '#004968',
+          '.text-primary-dark': {
+            color: 'var(--primary-dark)'
+          },
+          '.bg-primary-dark': {
+            'background-color': 'var(--primary-dark)'
+          }
+        }
+      }
+    ],
+    // themes: ['dark', 'corporate', 'night'],
+    darkTheme: 'dark', // name of one of the included themes for dark mode
+    base: true,
+    utils: true
+  }
 }
