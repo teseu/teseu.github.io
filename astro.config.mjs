5e29ba022cd2b9187fa5a14079985d39da1789e8
Tulio Macedo <3169307+teseu@users.noreply.github.com>
2025-10-18 10:34:20 -0300

    :art: atualização das bibliotecas
    Atualizações:
     - astro
     - tailwind
     - daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teseu.github.io',
-  experimental: {
-    i18n: {
-      defaultLocale: 'pt-br',
-      locales: ['pt-br', 'en'],
-    },
+  i18n: {
+    defaultLocale: 'pt-br',
+    locales: ['pt-br', 'en'],
   },
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
